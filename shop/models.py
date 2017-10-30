2715d1f976cfd76d8c656e24763bae5dc3e2fb57
Jude Ibe <judeibe@gmail.com>
2017-10-30 16:03:28 -0500

    Product Admin

--- shop/models.py
+++ shop/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Products(models.Model):
+class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
