ddc4c7575d753bf1fbb346f1af31fd487124fdef
Jude Ibe <judeibe@gmail.com>
2017-10-30 15:46:32 -0500

    added shop to settings.py
    fixed import errors

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-class Products(models.model):
-    name = models.CharField(blank=True, max_length=100, db_index=true)
+class Products(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField(blank=True)
     image = models.ImageField(upload_to="products/%Y/%m/%d")
