d78aa84d06761e42f9c2405df5f9e0fdaf613a63
Jude Ibe <judeibe@gmail.com>
2017-11-02 17:09:14 -0500

    models in their own class

--- shop/models/product.py
+++ shop/models/product.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -7,7 +8,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to="products/%Y/%m/%d")
 
     class Meta:
-        ordering= ('name',)
+        ordering = ('name',)
 
     def __str__(self):
         return self.name
