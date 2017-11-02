70c3a0e0a574c11afb8f086151c43daace9dac52
Jude Ibe <judeibe@gmail.com>
2017-11-02 17:35:14 -0500

    fix Product __str__ test

--- shop/tests/__init__.py
+++ shop/tests/__init__.py
@@ -1,7 +1,1 @@
-from django.test import TestCase
-from shop.models import Product
-
-
-class ProductModelTest(TestCase):
-    def test_string_representation(self):
-        self.fail("TODO Test Incomplete")
+from .product import ProductModelTest
