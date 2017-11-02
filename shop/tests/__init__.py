917ff351a67dc41afe06aadec0e0cc5b06f80c20
Jude Ibe <judeibe@gmail.com>
2017-11-02 17:25:38 -0500

    add test for Product __str__

---
+++ shop/tests/__init__.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from shop.models import Product
+
+
+class ProductModelTest(TestCase):
+    def test_string_representation(self):
+        self.fail("TODO Test Incomplete")
