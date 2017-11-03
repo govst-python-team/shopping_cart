55aa8c5befb7f397c08fe6430fb384c60dc14ab7
Jude Ibe <judeibe@gmail.com>
2017-11-03 12:20:45 -0500

    Revert "Feature/product model"

--- shop/admin.py
+++ shop/admin.py
@@ -1,6 +1,1 @@
-from django.contrib import admin
-from .models import Product
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name', 'price', 'description']
-admin.site.register(Product, ProductAdmin)
+# Register your models here.
--- shop/models.py
+++ shop/models.py
@@ -1,13 +1,1 @@
-from django.db import models
-
-class Product(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    description = models.TextField(blank=True)
-    image = models.ImageField(upload_to="products/%Y/%m/%d")
-
-    class Meta:
-        ordering= ('name',)
-
-    def __str__(self):
-        return self.name
+# Create your models here.
