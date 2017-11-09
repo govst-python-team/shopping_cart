a5f6ee65893c299e1aeed271c7be608e07d02d0f
Jude Ibe <judeibe@gmail.com>
2017-11-09 17:29:04 -0600

    product detail view

--- shop/models/product.py
+++ shop/models/product.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 
@@ -12,3 +13,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                       args=[self.id])
