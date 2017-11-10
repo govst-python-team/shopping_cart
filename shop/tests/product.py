3b65211a586304f70df3574dc15df6e4c2dbf154
Jude Ibe <judeibe@gmail.com>
2017-11-09 18:15:40 -0600

    product detail test

--- shop/tests/product.py
+++ shop/tests/product.py
@@ -1,8 +1,24 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 from shop.models import Product
+from shop.views import product_detail
 
 
 class ProductModelTest(TestCase):
     def test_string_representation(self):
         product = Product(name="Snakes on a Plane")
         self.assertEqual(str(product), product.name)
+
+
+class ProductDetailView(TestCase):
+    def test_product_id_1_resolves_to_harry_potter(self):
+        found = resolve('/1/')
+        self.assertEqual(found.func, product_detail)
+
+    def test_product_id_1_returns_the_correct_html(self):
+        request = HttpRequest()
+        response = product_detail(request, 1)
+        self.assertTrue(response.content.startswith(b'<html>'))
+        self.assertIn(b'<title>Products</title>', response.content)
+        self.assertIn(b'<h1>Harry Potter and the Chamber of Secrets</h1>', response.content)
