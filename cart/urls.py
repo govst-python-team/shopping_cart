9309483d55cee1fced428be1d9f29e5739b7ad8e
Jude Ibe <judeibe@gmail.com>
2017-11-19 18:58:51 -0600

    code layout and typos

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
-from . import views
 
+from . import views
 
 urlpatterns = [
     url(r'^$', views.cart_detail, name='cart_detail'),
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
+
 from shop.models import Product
 from .cart import Cart
 from .forms import CartAddProductForm
+
 
 @require_POST
 def cart_add(request, product_id):
@@ -16,11 +18,13 @@ def cart_add(request, product_id):
                  update_quantity=cd['update'])
     return redirect('cart:cart_detail')
 
-ef cart_remove(request, product_id):
+
+def cart_remove(request, product_id):
     cart = Cart(request)
     product = get_object_or_404(Product, id=product_id)
     cart.remove(product)
     return redirect('cart:cart_detail')
+
 
 def cart_detail(request):
     cart = Cart(request)
