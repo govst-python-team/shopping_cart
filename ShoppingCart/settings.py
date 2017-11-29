f158e4eceb0ff7f1e0a6769f5919392c65cd462c
Jude Ibe <judeibe@gmail.com>
2017-11-29 12:33:39 -0600

    css

--- ShoppingCart/settings.py
+++ ShoppingCart/settings.py
@@ -100,7 +100,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Session Data
 CART_SESSION_ID = 'cart'
